bb83d30d623007240e44832ac7a8a9df62edf718
Darijan Jelušić <darijan.jelusic@gmail.com>
2023-06-29 21:00:13 +0200

    Implement Mailjet

--- send_test_email.py
+++ send_test_email.py
@@ -2,16 +2,36 @@
 
 # Imports
 import requests
+from mailjet_rest import Client
+import os
+
+API_KEY = os.environ['MJ_APIKEY_PUBLIC']
+API_SEC = os.environ['MJ_APIKEY_PRIVATE']
+
+mailjet = Client(auth=(API_KEY, API_SEC))
 
 def send_simple_message():
     print("I am sending an email.")
-    return requests.post(
-        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
-        auth=("api", "YOUR_API_KEY"),
-        data={"from": 'hello@example.com',
-            "subject": "Visitor Alert",
-            "html": "<html> Your Raspberry Pi recognizes someone. </html>"})
-                      
-request = send_simple_message()
-print ('Status: '+format(request.status_code))
-print ('Body:'+ format(request.text))
+    data = {
+        'Messages': [
+            {
+              "From": {
+                "Email": "doorbell@mailjet.com",
+                "Name": "Smart Doorbell"
+              },
+              "To": [
+                {
+                  "Email": "djelusic@riteh.hr",
+                  "Name": "Admin"
+                }
+              ],
+              "Subject": "Mail test subject",
+              "TextPart": "Mail test",
+              "HTMLPart": "<h3>Mail Test HTML</h3>"
+            }
+        ]
+    }
+
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
